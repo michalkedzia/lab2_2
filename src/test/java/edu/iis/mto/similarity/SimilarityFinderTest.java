d6e0a9dbe86a6822d02a2a4227ad5e02e629d28e
michalkedzia <222463@edu.p.lodz.pl>
2021-03-22 20:14:35 +0100

    Fixing formating and gitignore

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -1,14 +1,5 @@
 package edu.iis.mto.similarity;
 
-import static org.junit.jupiter.api.Assertions.fail;
-
-import org.junit.jupiter.api.Test;
-
 class SimilarityFinderTest {
-
-    @Test
-    void test() {
-        fail("Not yet implemented");
-    }
-
+    
 }
