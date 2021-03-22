eb11569f9a4a3b5ebf48ba2c02c84103c9a6a61e
michalkedzia <222463@edu.p.lodz.pl>
2021-03-22 20:40:56 +0100

    Add tests with difrent seqs sizes

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -33,4 +33,25 @@ class SimilarityFinderTest {
     double result = similarityFinder.calculateJackardSimilarity(seq2, seq1);
     Assertions.assertEquals(result, 0.0d, 0.0d);
   }
+
+    @Test
+    void similarityFinder_should_return_1_when_sequence_are_the_same () {
+        SearchResult build = SearchResult.builder().withFound(true).withPosition(0).build();
+        SimilarityFinder similarityFinder = new SimilarityFinder((elem, sequence) -> build);
+        int[] seq = {1,2,3};
+        double result = similarityFinder.calculateJackardSimilarity(seq, seq);
+        Assertions.assertEquals(result, 1.0d, 0.0d);
+    }
+
+    @Test
+    void similarityFinder_should_return_0_5_when_one_sequence_contains_other () {
+        SearchResult build = SearchResult.builder().withFound(true).withPosition(0).build();
+        SimilarityFinder similarityFinder = new SimilarityFinder((elem, sequence) -> build);
+        int[] seq1 = {1,2,3,4,5,6};
+        int[] seq2 = {1,2,3};
+        double result = similarityFinder.calculateJackardSimilarity(seq2, seq1);
+        Assertions.assertEquals(result, 0.5d, 0.0d);
+    }
+
+
 }
